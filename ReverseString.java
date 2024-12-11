ff8aa99a3cc71d8ed2a25cd41b83fc1d0716adf1
Allie Lopez <alopez2@css.edu>
2024-12-11 17:55:42 -0600

    save to main

---
+++ ReverseString.java
@@ -0,0 +1,29 @@
+Package reverseString;
+
+public class ReverseString
+{
+    public static void main(String[] args) 
+    {
+        String word = "Awesome";
+        
+    }
+
+    public String reverse ( String word)
+    {
+        
+
+        
+        for (int i = 0; i < word.length()/2; i++)
+        {
+            //might need to make a temp to swap the values at each spot
+            char first = word.charAt(i);
+            char last = word.charAt(word.length() - i); 
+
+
+
+            
+        }
+
+
+    }
+}
