f7d3cf595e8af34c687bfe9c0bd624041186f57e
Allie Lopez <alopez2@css.edu>
2024-12-11 18:10:24 -0600

    current

---
+++ ReverseString.java
@@ -0,0 +1,16 @@
+Package reverseString;
+
+public class ReverseString
+{
+    public static void main(String[] args) 
+    {
+        String word = "Awesome";
+        for(int i = 0; i < word.length(); i--)
+        {
+            char first = word.charAt(i);
+            char last = word.charAt(word.length() - 1);
+
+            //create a temp to swap values 
+        }
+    }
+}
